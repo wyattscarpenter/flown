03bf623e0ac27c90fec8e288db38922f6aac211a
jasongray1649 <jasongray1649@gmail.com>
2025-12-31 21:50:43 +0000

    Finish `124.fln`: Rule 124 cellular automaton in pure FLOWN (#1)
    * Rewrote existing 124.fln to implement proposed new encoding. Implemented initial read after translation to create iterator according to new scheme detailed in brief description of implementation.
    
    * Added debugger at line 500 that goes to separator from anywhere, then prints all characters on the tape until four blanks in a row. Then exits.
    
    * Made end of program output 3 blanks before running the print debug, in order to distinguish from exiting via debugger or exiting via reaching the end.
    
    * Additional debugging lines
    
    * Skeletoned in rest of code for algorithm to loop.
    
    * Implemented read third cell for all cases
    
    * Made comments of skeleton clearer. Tested 2000's
    
    * Wrote writer for #000.
    
    * Commented todo's
    
    * Implemented Write & Print for all eight cases
    
    * Implemented write/print of final two cells. Tested without looping, and it correctly prints the first pass of the algorithm.
    
    * Loops at 4600 but I can't get the output to display when looping.
    
    * Modified test 124 to output a finite file. Seems like everything works.
    
    * Removed debugging statement that prints initial input.

--- test_124.sh
+++ test_124.sh
@@ -1,3 +1,3 @@
-#echo adds a newline to the end, implicitly
-cc flown.c -o flown && echo 1010101001 | ./flown 124.fln 2>&1 | less
+cc flown.c -o flown
+echo 1001010001 | ./flown 124.fln 2>&1 >> output.txt & sleep 1
 rm flown
